8e380f0ac3080fb901e40123e6a91a1a9b32c5a9
Otto Ludescher <ludescher.otto@gmail.com>
2022-11-05 17:11:50 +0100

    központi helyen a címke kiiratás

--- resources/views/forums/tags.blade.php
+++
@@ -1,9 +0,0 @@
-@unless($forum->tags->isEmpty())
-<p>
-<b>Címék: </b>
-@for ($i = 0; $i < $last=count($forum->tags)-1; $i++)
-<a href="{{ url('forum')}}/cimke/{{$forum->tags[$i]->id }}/{{$forum->tags[$i]->slug}}">{{ $forum->tags[$i]->name }}</a>,
-@endfor
-<a href="{{ url('forum')}}/cimke/{{$forum->tags[$last]->id }}/{{$forum->tags[$last]->slug}}">{{ $forum->tags[$last]->name }}</a>
-</p>
-@endunless
