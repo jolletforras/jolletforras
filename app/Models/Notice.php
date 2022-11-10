1e0345582a2716290b68952417783facbaad8587
Otto Ludescher <ludescher.otto@gmail.com>
2022-11-10 14:36:20 +0100

    user() felvétele

--- app/Models/Notice.php
+++ app/Models/Notice.php
@@ -18,6 +18,11 @@ class Notice extends Model
         'notifiable_id', 'user_id', 'type', 'comment_id', 'email_sent', 'ask_notice'
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function scopeFindBy($query,$notifiable_id,$user_id,$type)
     {
         $query->where('notifiable_id',$notifiable_id)->where('user_id',$user_id)->where('type', $type);
