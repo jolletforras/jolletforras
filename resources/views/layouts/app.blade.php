a233a6d937cf8f0008b118691bc792370b110fe4
foldv <foldvari.robert@gmail.com>
2022-11-14 18:29:24 +0100

    footer hozzáadás

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -142,7 +142,13 @@
 
 
 	    </div>
+		<footer class="footer">
+			© 2022 Kontaktia Kft. – Minden jog fenntartva!    <a href="{{ url('/adatkezeles') }}">Adatkezelési tájékoztató</a>
+		</footer>
 	</div>
+
+
+
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -152,7 +158,6 @@
 
 
     @yield('footer')
-
 
 
 
