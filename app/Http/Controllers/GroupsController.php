bbe5f307bc8636d7a87b81539c054d0762b41f5c
Otto Ludescher <ludescher.otto@gmail.com>
2022-11-21 17:31:59 +0100

    a cron_test ürítése

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -428,67 +428,7 @@ class GroupsController extends Controller
 
     public function cron_test()
     {
-        $notices = Notice::where('email_sent', 0)->oldest('updated_at')->get();
-
-        $nr = 1;
-        foreach ($notices as $notice) {
-            if($nr>50) exit;
-
-            $notifiable_class = "\\App\Models\\".$notice->type;
-
-            $notifiable = $notifiable_class::find($notice->notifiable_id);
-            $user = User::find($notice->user_id);
-
-            //ha valamelyikük nem található meg, akkor törli az értesítést
-            if(is_null($notifiable) || is_null($user) ) {
-                $notice->delete();
-                continue;
-            }
-
-            $group = Group::find($notifiable->group_id);
-            if(is_null($group)) {
-                $notice->delete();
-                continue;
-            }
-
-            $data['name'] = $user->name;
-            $data['group_name'] = $group->name;
-            $data['theme_title'] = $notifiable->title;
-            $data['theme_url'] = 'csoport/' . $group->id . '/' . $group->slug . '/tema/' . $notifiable->id . '/' . $notifiable->slug;
-            $data['email'] = $user->email;
-            $data['user_id'] = $user->id;
-            $data['theme'] = $notifiable->body;
-
-            //ezt már nem küldi ki újból
-            $notice->update(['email_sent' => 1]);
-
-            if($notice->comment_id==0) {     //új téma
-                $data['author_name'] = $notifiable->user->name;
-                $email_template = 'groupthemes.new_theme_email';
-                $data['subject'] = "Új téma a(z) '". $group->name."' csoportodban";
-            }
-            else {                           //hozzászólás értesítés
-                $comment = Comment::find($notice->comment_id);
-                //ha nem találja a hozzászólást, akkor továbblép
-                if(is_null($comment)) {
-                    continue;
-                }
-                $data['author_name'] = $comment->commenter->name;
-                $data['comment'] = $comment->body;
-                $data['subject'] = "Új hozzászólás a(z) '".$notifiable->title."' beszélgetésben";
-                $email_template = 'groupthemes.new_comment_email';
-            }
-
-            Mail::send($email_template, $data, function ($message) use ($data) {
-                $message->from('tarsadalmi.jollet@gmail.com', "tarsadalmijollet.hu");
-                $message->subject($data['subject']);
-                $message->to($data['email']);
-            });
-
-            sleep(3);
-
-            $nr++;
-        }
+        //tesztelés esetén ide másold a SendNoticeEmails.php-ból
     }
 
 }
