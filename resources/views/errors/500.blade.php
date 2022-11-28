a5afaedbb853c68e939b74091a2291d463da2ea6
Otto Ludescher <ludescher.otto@gmail.com>
2022-11-28 12:25:51 +0100

    500-as szerver error esetén kiírt üzenet beállítása

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <ul class="alert alert-warning">
+        Sajnos valami hiba történt. <i class="fa fa-frown-o" aria-hidden="true"></i><br/>
+        Hamarosan javítjuk, amihez kérjük a segítséged. Kérlek jelezd a hibát a tarsadalmi.jollet@gmail.com email címen. Köszönjük! <i class="fa fa-smile-o" aria-hidden="true"></i>
+    </ul>
+@endsection
