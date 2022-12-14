24089f14f25d7c045785300d84307e7015cf704d
Otto Ludescher <ludescher.otto@gmail.com>
2022-12-14 14:23:37 +0100

    hírek, írás oldal kinézetén alakítás

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,17 +6,13 @@
 @endsection
 
 @section('content')
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h2>{{ $article->title }}</h2>
-		</div>
-        <div class="panel-body">
-			@if (Auth::check() && Auth::user()->id==$article->user->id)
-				<a href="{{url('iras')}}/{{$article->id}}/{{$article->slug}}/modosit" type="submit" class="btn btn-default">Módosít</a>
-			@endif
-			{!! $article->body !!}
-			@include('partials.author', ['author'=>'','obj'=>$article])
-	    </div>
+	<div class="inner_box" style="font-size: 16px;">
+		<h2>{{ $article->title }}</h2>
+		@if (Auth::check() && Auth::user()->id==$article->user->id)
+			<a href="{{url('iras')}}/{{$article->id}}/{{$article->slug}}/modosit" type="submit" class="btn btn-default">Módosít</a>
+		@endif
+		{!! $article->body !!}
+		@include('partials.author', ['author'=>'','obj'=>$article])
     </div>
 @endsection
 
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -6,18 +6,14 @@
 @endsection
 
 @section('content')
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h2>{{ $news->title }}</h2>
-		</div>
-        <div class="panel-body">
-			@if (Auth::check() && Auth::user()->id==$news->user->id)
-				<a href="{{url('hir')}}/{{$news->id}}/{{$news->slug}}/modosit" type="submit" class="btn btn-default">Módosít</a>
-			@endif
-			{!! $news->body !!}
-			@include('partials.tags',['url'=>'hir','obj'=>$news])
-			@include('partials.author', ['author'=>'','obj'=>$news])
-	    </div>
-    </div>
+	<div class="inner_box" style="font-size: 16px;">
+		<h2>{{ $news->title }}</h2>
+		@if (Auth::check() && Auth::user()->id==$news->user->id)
+			<a href="{{url('hir')}}/{{$news->id}}/{{$news->slug}}/modosit" type="submit" class="btn btn-default">Módosít</a>
+		@endif
+		{!! $news->body !!}
+		@include('partials.tags',['url'=>'hir','obj'=>$news])
+		@include('partials.author', ['author'=>'','obj'=>$news])
+	</div>
 @endsection
 
