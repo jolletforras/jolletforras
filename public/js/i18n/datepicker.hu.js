6cff9dd06c62e5c9d40093f21560807330d53114
Otto Ludescher <ludescher.otto@gmail.com>
2023-02-07 20:08:47 +0100

    dátum kiválasztó

---
+++ public/js/i18n/datepicker.hu.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['hu'] = {
+    days: ['Vasárnap', 'Hétfö', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat'],
+    daysShort: ['Vas', 'Hét', 'Ked', 'Sze', 'Csü', 'Pén', 'Szo'],
+    daysMin: ['V', 'H', 'K', 'Sze', 'Cs', 'P', 'Szo'],
+    months: ['Január', 'Február', 'Március', 'Április', 'Május', 'Június','Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Már', 'Ápr', 'Máj', 'Jún','Júl', 'Aug', 'Szep', 'Okt', 'Nov', 'Dec'],
+    today: 'Ma',
+    clear: 'Törlés',
+    dateFormat: 'yyyy.mm.dd',
+    timeFormat: 'hh:ii aa',
+    firstDay: 1
+}; })(jQuery);
