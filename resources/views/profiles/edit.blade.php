d9e59b7173ea388191adcb1a748ef5e96d7a332f
Otto Ludescher <ludescher.otto@gmail.com>
2023-02-10 13:56:37 +0100

    a rules alkalmazása miatt old használata minden mezőre, de nem aktiválódik mert a mezőkre frontend-en maxlength be van állítva

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -64,21 +64,21 @@
 
 						<div class="form-group">
 							<label for="name">Teljes név*:</label>
-							<input class="form-control" maxlength="30" required="required" name="name" type="text" value="{{$user->name}}" id="name">
+							<input class="form-control" maxlength="30" required="required" name="name" type="text" value="{{old('name',$user->name)}}" id="name">
 						</div>
 
 						<div class="form-group">
  							<b>Lakóhely*</b>
 							<a href="#location_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 							<div id="location_info" class="collapse info">Budapest esetén a kerületet add meg itt</div>
-							<input class="form-control" maxlength="30" name="location" type="text" value="{{$user->location}}">
+							<input class="form-control" maxlength="30" name="location" type="text" value="{{old('location',$user->location)}}">
 						</div>	
 
 						<div class="form-group">
 							<b>Irányítószám*</b>
 							<a href="#zip_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 							<div id="zip_info" class="collapse info">A térképre való elhelyezésed miatt szükséges</div>
-							<input class="form-control" maxlength="4" name="zip_code" type="text" value="{{$user->zip_code}}">
+							<input class="form-control" maxlength="4" name="zip_code" type="text" value="{{old('zip_code',$user->zip_code)}}">
 						</div>
 
 						<div class="form-group">
@@ -88,7 +88,7 @@
 							<select class="form-control" name="city">
 								<option value=""></option>
 								@foreach(constx('CITY') as $key => $val)
-									<option value="{{ $key }}" @if($user->city==$val) selected @endif>{{ $val }}</option>
+									<option value="{{ $key }}" @if(old('city',$user->city)==$val) selected @endif>{{ $val }}</option>
 								@endforeach
 							</select>
 						</div>
@@ -97,14 +97,14 @@
 							<label for="webpage_name">Weboldalad neve</label>
 							<a href="#webpage_name_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 							<div id="webpage_name_info" class="collapse info">Ha van saját weboldalad, amit szívesen megmutatnál a portálon regisztráltaknak, írd ide. Ha több oldalad is van, akkor egymástól vesszővel elválasztva add meg.</div>
-							<input class="form-control" maxlength="200" name="webpage_name" type="text" value="{{$user->webpage_name}}" id="webpage_name">
+							<input class="form-control" maxlength="200" name="webpage_name" type="text" value="{{old('webpage_name',$user->webpage_name)}}" id="webpage_name">
                         </div>
 
                         <div class="form-group">
 							<label for="webpage_url">Weboldalad linkje</label>
 							<a href="#webpage_url_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 							<div id="webpage_url_info" class="collapse info">A böngésződ szerkesztőlécéből másold be a hivatkozást. Ha több oldalad van, akkor a linkeket egymástól vesszővel választva add meg. Ugyanabban a sorrendbe tedd meg, ahogy a weboldalaid nevét adtad meg.</div>
-							<input class="form-control" maxlength="200" name="webpage_url" type="text" value="{{$user->webpage_url}}" id="webpage_url">
+							<input class="form-control" maxlength="200" name="webpage_url" type="text" value="{{old('webpage_url',$user->webpage_url)}}" id="webpage_url">
                         </div>
 
 						<div class="form-group">
@@ -113,7 +113,7 @@
 							<div id="intro_info" class="collapse info">
 								Mi mindent csináltál eddig és mivel foglalkozol most? Írj bátran mindarról, amit erre a portálra tartozónak gondolsz.
 							</div>
-							<textarea class="form-control" rows="5" name="introduction" cols="50">{{$user->introduction}}</textarea>
+							<textarea class="form-control" rows="5" name="introduction" cols="50">{{old('introduction',$user->introduction)}}</textarea>
 						</div>
 
 						<div class="form-group">
@@ -122,14 +122,14 @@
 							<div id="intention_info" class="collapse info">
 								Mit szeretnél itt csinálni, milyen reményeid vannak az oldallal kapcsolatban?
 							</div>
-							<textarea class="form-control" rows="5" name="intention" cols="50">{{$user->intention}}</textarea>
+							<textarea class="form-control" rows="5" name="intention" cols="50">{{old('intention',$user->intention)}}</textarea>
 						</div>
 
 						<div class="form-group">
 							<b>Mi lelkesít, amivel a következő hónapokban foglalkozni szeretnél?*</b>
 							<a href="#interest_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
 							<div id="interest_info" class="collapse info">Itt írhatsz konkrét kezdeményezéseidről, terveidről, amelyekkel a társadalmi jólléthez szeretnél hozzájárulni.</div>
-							<textarea class="form-control" rows="5" name="interest" cols="50">{{$user->interest}}</textarea>
+							<textarea class="form-control" rows="5" name="interest" cols="50">{{old('interest',$user->interest)}}</textarea>
 						</div>
 
 						<div class="form-group">
@@ -138,7 +138,7 @@
 							<div id="skill_info" class="collapse info">Miben van jártasságod, mit tudnál hozzáadni egy közösséghez? Címkét úgy tudsz felvenni, hogy elkezded írni amit fel szeretnél venni. A megjelenő listában rákattintasz a megfelelőre. Ha nincs olyan, akkor írd be teljesen a címkéd, majd kattints a listában rá.</div>
 							<select id="skill_tag_list" name="skill_tag_list[]" class="form-control" multiple>
 								@foreach($skill_tags as $key => $val)
-									<option value="{{ $key }}" @if(isset($selected_skill_tags) && in_array($key,$selected_skill_tags)) selected @endif>{{ $val }}</option>
+									<option value="{{ $key }}" @if(isset($selected_skill_tags) && in_array($key,old('skill_tag_list',$selected_skill_tags))) selected @endif>{{ $val }}</option>
 								@endforeach
 							</select>
 						</div>
@@ -149,13 +149,13 @@
 							<div id="interest_tag_info" class="collapse info">Miben szeretnéd elmélyíteni a tudásod ami a közösség szolgálatára lehet? Címkét úgy tudsz felvenni, hogy elkezded írni amit fel szeretnél venni. A megjelenő listában rákattintasz a megfelelőre. Ha nincs olyan, akkor írd be teljesen a címkéd, majd kattints a listában rá.</div>
 							<select id="interest_tag_list" name="interest_tag_list[]" class="form-control" multiple>
 								@foreach($interest_tags as $key => $val)
-									<option value="{{ $key }}" @if(isset($selected_interest_tags) && in_array($key,$selected_interest_tags)) selected @endif>{{ $val }}</option>
+									<option value="{{ $key }}" @if(isset($selected_interest_tags) && in_array($key,old('interest_tag_list',$selected_interest_tags))) selected @endif>{{ $val }}</option>
 								@endforeach
 							</select>
 						</div>
 
 						<div class="form-group">
-							<input name="public" type="checkbox" value="{{$user->public}}" @if($user->public) checked @endif>
+							<input name="public" type="checkbox" value="1" @if((!old() && $user->public) || old('public')) checked @endif>
 							<span style="padding-top: 10px; font-size: 18px;">Nyilvános</span>
 							<a href="#public_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a></label>
 							<div id="public_info" class="collapse info">Ha nyilvános a bemutatkozásod, akkor nem regisztráltak számára is láthatóvá válik.</div>
