db3ee0f3f8c15f0524bbca0b061435ce93dd4fcd
Otto Ludescher <ludescher.otto@gmail.com>
2023-02-10 13:48:46 +0100

    6 címkénél többet nem enged kiválasztani

--- resources/views/partials/add_tag_script.blade.php
+++ resources/views/partials/add_tag_script.blade.php
@@ -1,11 +1,23 @@
 			<script>
 				$('#skill_tag_list').select2({
 					placeholder: 'Válasz egy címkét vagy vegyél fel ha nincs olyan',
-					tags: true
+					tags: true,
+					maximumSelectionLength: 6,
+					language: {
+						maximumSelected: function (e) {
+							return "Legfeljebb " + e.maximum + " címkét választhatsz";
+						}
+					}
 				});
 
 				$('#interest_tag_list').select2({
 					placeholder: 'Válasz egy címkét vagy vegyél fel ha nincs olyan',
-					tags: true
+					tags: true,
+					maximumSelectionLength: 6,
+					language: {
+						maximumSelected: function (e) {
+							return "Legfeljebb " + e.maximum + " címkét választhatsz";
+						}
+					}
 				});
 			</script>
