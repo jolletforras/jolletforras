01cf1a672afb8010df13f3ae3267eafbb7adf229
Otto Ludescher <ludescher.otto@gmail.com>
2023-02-10 14:22:26 +0100

    30 karakternél hosszabb címkét nem enged felvenni

--- resources/views/partials/add_tag_script.blade.php
+++ resources/views/partials/add_tag_script.blade.php
@@ -1,23 +1,13 @@
 			<script>
-				$('#skill_tag_list').select2({
+				$('.tag-list').select2({
 					placeholder: 'Válasz egy címkét vagy vegyél fel ha nincs olyan',
 					tags: true,
 					maximumSelectionLength: 6,
+					maximumInputLength: 30,
 					language: {
-						maximumSelected: function (e) {
-							return "Legfeljebb " + e.maximum + " címkét választhatsz";
-						}
-					}
-				});
-
-				$('#interest_tag_list').select2({
-					placeholder: 'Válasz egy címkét vagy vegyél fel ha nincs olyan',
-					tags: true,
-					maximumSelectionLength: 6,
-					language: {
-						maximumSelected: function (e) {
-							return "Legfeljebb " + e.maximum + " címkét választhatsz";
-						}
+						maximumSelected: function (e) {	return "Legfeljebb " + e.maximum + " címkét választhatsz";	},
+						noResults: function() {	return "Nincs találat";	},
+						inputTooLong: function(e) {	return "Kérlek rövidebb címkét adj meg, mert a címke hossza maximum 30 karakter lehet!";	},
 					}
 				});
 			</script>
