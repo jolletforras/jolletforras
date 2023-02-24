2a4d86fc4f0c30bdcd71ec93cdc7d4c1892112d6
Otto Ludescher <ludescher.otto@gmail.com>
2023-02-24 17:01:36 +0100

    szöveg módosítás

--- resources/views/groupnews/index.blade.php
+++ resources/views/groupnews/index.blade.php
@@ -17,7 +17,7 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="inner_box collapse" id="create_news_info" style="font-size: 18px">
-					Csoport kezelőként lehetőséged van a csoport aktuális történéseit, eseményeit egy-egy hírbejegyzésben összefoglalni.
+					Csoport kezelőként lehetőséged van a csoport aktuális történéseit, eseményeit egy-egy hírbejegyzésben összefoglalni. A láthatóságnál beállíthatod, hogy azok is értesülhessenek a közösséged legújabb híreiről, akik nem tagjai a csoportnak.
 				</div>
 			</div>
 		</div>
