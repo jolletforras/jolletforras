bbae245e7c323bd88297e6a7b29d8529eaaf8351
Otto Ludescher <ludescher.otto@gmail.com>
2023-02-26 09:54:29 +0100

    a szerző, létrehozás megjelenítésen változtatás

--- resources/views/partials/author.blade.php
+++ resources/views/partials/author.blade.php
@@ -1,4 +1,4 @@
 <p>
-    <i>{{$author}}@if(Auth::check() || $obj->user->public)<a href="{{ url('profil',$obj->user->id) }}/{{$obj->user->slug}}">{{ $obj->user->name }}</a>@else{{ $obj->user->name }}@endif, létrehozva: {{ $obj->created_at }}@if( $obj->updated_at!=$obj->created_at), módosítva:  {{ $obj->updated_at }}@endif
+    <i style="font-size: 12px; color: #494949; font-family: Arial;">{{$author}}@if(Auth::check() || $obj->user->public)<a href="{{ url('profil',$obj->user->id) }}/{{$obj->user->slug}}">{{ $obj->user->name }}</a>@else{{ $obj->user->name }}@endif, {{ $obj->created_at }}@if( $obj->updated_at!=$obj->created_at), módosítva:  {{ $obj->updated_at }}@endif
     </i>
 </p>
