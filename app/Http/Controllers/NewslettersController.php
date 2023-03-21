a0f93ff1ddc9b379fcb100fba99aa9d3439a975a
Otto Ludescher <ludescher.otto@gmail.com>
2023-03-21 15:51:08 +0100

    kis javítás

--- app/Http/Controllers/NewslettersController.php
+++ app/Http/Controllers/NewslettersController.php
@@ -51,8 +51,8 @@ class NewslettersController extends Controller
         $newsletter = Auth::user()->newsletters()->create([
             'title' => $request->get('title'),
             'meta_description' => $request->get('meta_description'),
-            'body' => $request->get('body'),
             'short_description' => $request->get('short_description'),
+            'body' => $request->get('body'),
             'slug' => Str::slug($request->get('title'))
         ]);
 
@@ -92,8 +92,8 @@ class NewslettersController extends Controller
         $newsletter->update([
             'title' => $request->get('title'),
             'meta_description' => $request->get('meta_description'),
-            'body' => $request->get('body'),
             'short_description' => $request->get('short_description'),
+            'body' => $request->get('body'),
             'slug' => Str::slug($request->get('title'))
         ]);
 
