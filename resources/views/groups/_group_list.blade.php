00a881e67b510ee6ab3b5caec9bb989bda368d3e
Otto Ludescher <ludescher.otto@gmail.com>
2023-03-21 23:30:35 +0100

    csoport listában cím a kép  fölött

--- resources/views/groups/_group_list.blade.php
+++ resources/views/groups/_group_list.blade.php
@@ -2,6 +2,14 @@
     <div class="col-12 col-sm-6 col-md-4 group">
         <div class="card">
             <div class="card-header"></div>
+            <div class="card-title">
+                <h3>
+                    <a href="{{ url('csoport',$group->id) }}/{{$group->slug}}">{{ $group->name }}</a>
+                    @if($group->city!='')
+                        - <i style="font-weight: normal; font-size: 16px;">{{$group->get_location()}}</i>
+                    @endif
+                </h3>
+            </div>
             <div class="image-box">
                 @if(file_exists(public_path('images/groups/'.$group->id.'.jpg')))
                 <div class="image" style="background-image:url('images/groups/{{$group->id}}.jpg');"></div>
@@ -11,12 +19,6 @@
             </div>
             <div class="card-body">
             @if(isset($group->user->id))
-                <h3>
-                    <a href="{{ url('csoport',$group->id) }}/{{$group->slug}}">{{ $group->name }}</a>
-                    @if($group->city!='')
-                        - <i style="font-weight: normal; font-size: 16px;">{{$group->get_location()}}</i>
-                    @endif
-                </h3>
                 <p>
                 @if(strlen($group->description)>800)
                     {!! nl2br(mb_substr($group->description,0,800)) !!}
