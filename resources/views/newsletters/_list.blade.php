201ea0245d70223d03e3c5ada4107217d9fb7d99
Otto Ludescher <ludescher.otto@gmail.com>
2023-03-21 16:00:46 +0100

    kivettem a szerző, dátumot

--- resources/views/newsletters/_list.blade.php
+++ resources/views/newsletters/_list.blade.php
@@ -4,7 +4,6 @@
         <tr>
             <td>
                 <h3><a href="{{ url('hirlevel',$newsletter->id) }}/{{$newsletter->slug}}">{{ $newsletter->title }}</a></h3>
-                <a href="{{ url('profil',$newsletter->user->id) }}/{{$newsletter->user->slug}}">{{ $newsletter->user->name }}</a>,	{{ $newsletter->updated_at }}
                 @if (Auth::check() && Auth::user()->admin)
                     <a href="{{url('hirlevel')}}/{{$newsletter->id}}/{{$newsletter->slug}}/modosit">módosít</a>
                 @endif
