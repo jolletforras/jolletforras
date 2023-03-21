6c92234470b2dc24f8faf6c03a761988438311a6
Otto Ludescher <ludescher.otto@gmail.com>
2023-03-21 16:02:02 +0100

    kivettem a létrehozót, és a dátumnál a létrehozási idő szerepel

--- resources/views/newsletters/show.blade.php
+++ resources/views/newsletters/show.blade.php
@@ -12,8 +12,8 @@
 			<h2>{{ $newsletter->title }}</h2>
 		</div>
         <div class="panel-body">
-        	<p><b>{{ $newsletter->user->name }}, {{ $newsletter->updated_at }}</b></p>
 			{!! $newsletter->body !!}
+			<p><b>{{ $newsletter->created_at }}</b></p>
 			@if (Auth::check() && Auth::user()->admin)
 			<a href="{{url('hirlevel')}}/{{$newsletter->id}}/{{$newsletter->slug}}/modosit" type="submit" class="btn btn-default">Módosít</a>
 			@endif
