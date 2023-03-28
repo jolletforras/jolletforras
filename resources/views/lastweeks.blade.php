fa88e4e04eeff9d5ad70dc1d50dbf0262adaf393
Otto Ludescher <ludescher.otto@gmail.com>
2023-03-28 21:19:46 +0200

    az új csoportokat írásokat nem kártyásan jelenítem meg

--- resources/views/lastweeks.blade.php
+++ resources/views/lastweeks.blade.php
@@ -18,7 +18,36 @@
     @endif
     @if($groups->isNotEmpty())
         <h3>Új csoportok</h3>
-        @include('groups._group_list')
+        @foreach ($groups as $group)
+        <div class="col-12">
+            <h3>
+                <a href="{{ url('csoport',$group->id) }}/{{$group->slug}}">{{ $group->name }}</a>
+                @if($group->city!='')
+                    - <i style="font-weight: normal; font-size: 16px;">{{$group->get_location()}}</i>
+                @endif
+            </h3>
+            <p style="text-align: center;">
+                @if(file_exists(public_path('images/groups/'.$group->id.'.jpg')))
+                    <img src="{{ url('/images/groups') }}/{{$group->id}}.jpg" style="max-width: 500px; max-height: 500px;">
+                @else
+                    <img src="{{ url('/images') }}/tarsadalmijollet.png" style="max-width: 500px; max-height: 500px;">
+                @endif
+            </p>
+            @if(isset($group->user->id))
+            <p>
+                @if(strlen($group->description)>800)
+                    {!! nl2br(mb_substr($group->description,0,800)) !!}
+                    <a href="{{ url('csoport',$group->id) }}/{{$group->slug}}">... tovább</a>
+                @else
+                    {!! nl2br($group->description) !!}
+                @endif
+            </p>
+                @if (Auth::check())
+            @include('partials.tags',['url'=>'csoport','obj'=>$group])
+                @endif
+            @endif
+        </div>
+        @endforeach
         <hr>
     @endif
     @if($forums->isNotEmpty())
@@ -28,7 +57,16 @@
     @endif
     @if($articles->isNotEmpty())
          <h3>Új írások</h3>
-         @include('articles._list')
+         @foreach ($articles as $article)
+         <div class="col-12">
+             <h3><a href="{{ url('iras',$article->id) }}/{{$article->slug}}">{{ $article->title }}</a></h3>
+             @if (Auth::check() && (Auth::user()->id==$article->user->id || Auth::user()->admin))
+                 <a href="{{url('iras')}}/{{$article->id}}/{{$article->slug}}/modosit" class="edit">módosít</a><br>
+             @endif
+             <p style="text-align: center;"><img src="{{ url('/images/posts') }}/{{ $article->image}}" style="max-width: 500px; max-height: 500px;"></p>
+             <p>{!! $article->short_description !!}</p>
+         </div>
+        @endforeach
          <hr>
     @endif
     @if($newsletters->isNotEmpty())
