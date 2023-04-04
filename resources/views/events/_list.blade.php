0d8119f304a04dfb547bd828da0dbac401587c84
Otto Ludescher <ludescher.otto@gmail.com>
2023-04-04 12:10:35 +0200

    megjeleníti a rövidített szöveget, ha van

--- resources/views/events/_list.blade.php
+++ resources/views/events/_list.blade.php
@@ -14,7 +14,16 @@
         @endif
     @endif
     <article>
-        <div class="body">{!! $event->body !!}</div>
+        <div class="body">
+            @if(isset($event->image))
+                <img class="img-responsive" style="display: block; margin-left: auto; margin-right: auto; max-height: 300px;" src="{{ url('/images/posts') }}/{{$event->image}}"/>
+            @endif
+            @if(isset($event->shorted_text))
+                {!! str_replace("#...#","<a href='".url('esemeny')."/".$event->id."/".$event->slug."'>... tovább</a>",$event->shorted_text) !!}
+            @else
+                {!! preg_replace("/<img[^>]+\>/i", "",$event->body) !!}
+            @endif
+        </div>
     </article>
     @if (Auth::check())
         <a href="{{ url('esemeny',$event->id) }}/{{$event->slug}}" type="submit" class="btn btn-default">Hozzászólok</a>
