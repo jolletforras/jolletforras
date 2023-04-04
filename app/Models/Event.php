98aabd85b0ac67c525dd2f610b065b4f64412c12
Otto Ludescher <ludescher.otto@gmail.com>
2023-04-04 11:21:05 +0200

    menthető a shorted_text és az image mező

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,7 +9,7 @@ use Auth;
 class Event extends Model
 {
     protected $fillable = [
-        'title','meta_description','expiration_date','body','type','slug','visibility','group_id','created_at','updated_at'
+        'title','meta_description','expiration_date','shorted_text','body','image','type','slug','visibility','group_id','created_at','updated_at'
     ];
 
     public function user()
