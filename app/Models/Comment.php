6edd37af39534f137fe75ff45396f479ed3023fb
Otto Ludescher <ludescher.otto@gmail.com>
2023-08-07 18:53:36 +0200

    hozzászólásnál nem dátumot ír, hanem percet, órát, napot, hetet, hónapot

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -36,4 +36,30 @@ class Comment extends Model
         //return (new Carbon($date))->format('Y-m-d');
         return Carbon::parse($date)->format('Y-m-d H:i');
     }
+
+    public function getSinceAttribute($date)
+    {
+        $now = Carbon::now();
+        $created_at = Carbon::parse($this->created_at);
+
+        $minutes = $created_at->diffInMinutes($now);
+
+        if($minutes<60) {
+            $since = $created_at->diffInMinutes($now).' p.' ;
+        }
+        elseif ($minutes<1440) {  //24*60 = 1 nap
+            $since = $created_at->diffInHours($now).' ó.';
+        }
+        elseif ($minutes<10080) { //24*60*7 = 1 hét
+            $since = $created_at->diffInWeeks($now).' n.';
+        }
+        elseif ($minutes<=40320) { //24*60*7*4 = 4 hét
+            $since = $created_at->diffInWeeks($now).' hete';
+        }
+        else {
+            $since = $created_at->diffInMonths($now).' hónapja';
+        }
+
+        return $since;
+    }
 }
