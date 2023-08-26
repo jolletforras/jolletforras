5d0dc3157bfc1e3359960be9ecf5e1b5114ef709
Otto Ludescher <ludescher.otto@gmail.com>
2023-08-26 16:37:27 +0200

    új oldalon nyitja meg

--- resources/views/profiles/partials/groups.blade.php
+++ resources/views/profiles/partials/groups.blade.php
@@ -2,11 +2,8 @@
     <h4>
         <b>Csoportjaim: </b>
         @for ($i = 0; $i < $last=count($user->member_of_groups)-1; $i++)
-            <a href="{{ url('csoport',$user->member_of_groups[$i]->id) }}/{{$user->member_of_groups[$i]->slug}}">
-                {{$user->member_of_groups[$i]->name}}</a>,&nbsp;
+            <a href="{{ url('csoport',$user->member_of_groups[$i]->id) }}/{{$user->member_of_groups[$i]->slug}}" target="_blank">{{$user->member_of_groups[$i]->name}}</a>,&nbsp;
         @endfor
-        <a href="{{ url('csoport',$user->member_of_groups[$last]->id) }}/{{$user->member_of_groups[$last]->slug}}">
-            {{$user->member_of_groups[$last]->name}}
-        </a>
+        <a href="{{ url('csoport',$user->member_of_groups[$last]->id) }}/{{$user->member_of_groups[$last]->slug}}" target="_blank">{{$user->member_of_groups[$last]->name}}</a>
     </h4>
 @endunless
