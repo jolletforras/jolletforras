1bcd75a3e2fe93540d4cf13713d823efbaccbfdc
Otto Ludescher <ludescher.otto@gmail.com>
2023-09-04 22:51:40 +0200

    a módosít szót felvittem a címhez, hogy hosszabb cím esetén ne két sorral legyen lejebb

--- resources/views/articles/_list.blade.php
+++ resources/views/articles/_list.blade.php
@@ -7,10 +7,12 @@
                 <div class="image" style="background-image:url('images/posts/{{$article->image}}');"></div>
             </div>
             <div class="card-body">
-                <h3><a href="{{ url('iras',$article->id) }}/{{$article->slug}}">{{ $article->title }}</a></h3>
-                @if (Auth::check() && (Auth::user()->id==$article->user->id || Auth::user()->admin))
-                    <a href="{{url('iras')}}/{{$article->id}}/{{$article->slug}}/modosit" class="edit">módosít</a><br>
-                @endif
+                <h3>
+                    <a href="{{ url('iras',$article->id) }}/{{$article->slug}}">{{ $article->title }}</a>
+                    @if (Auth::check() && (Auth::user()->id==$article->user->id || Auth::user()->admin))
+                        <a href="{{url('iras')}}/{{$article->id}}/{{$article->slug}}/modosit" class="edit">módosít</a><br>
+                    @endif
+                </h3>
                 <p>{!! $article->short_description !!}</p>
             </div>
         </div>
