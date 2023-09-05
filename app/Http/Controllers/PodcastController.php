bbe1fc44597185c77c2d9a11674796781d3c5c86
Otto Ludescher <ludescher.otto@gmail.com>
2023-09-05 22:24:21 +0200

    nem bejelentkezve is meg lehet nyitni az egyéni oldalt

--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -11,7 +11,7 @@ class PodcastController extends Controller
 {
 
     public function __construct() {
-        $this->middleware('auth', ['except'=>['index']]);
+        $this->middleware('auth', ['except'=>['index','show']]);
     }
 
 
