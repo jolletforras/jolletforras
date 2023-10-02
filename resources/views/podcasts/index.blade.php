7f611585881e80a5ffb033decfa49f172628a1c3
Otto Ludescher <ludescher.otto@gmail.com>
2023-10-02 19:39:30 +0200

    változtatás a nyitószövegen

--- resources/views/podcasts/index.blade.php
+++ resources/views/podcasts/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 	<div class="row narrow-page">
 		<div class="col-sm-6">
-			<h2>Az új világ hangjai</h2><a href="#podcast_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-left: 4px;"></i></a>
+			<h2>Az új világ hangjai</h2>
 		</div>
 		<div class="col-sm-6 text-right">
 			@if (Auth::check() && Auth::user()->admin)
@@ -15,12 +15,29 @@
 			@endif
 		</div>
 	</div>
-	<div class="row narrow-page">
+	<div class="inner_box narrow-page">
+		<div class="body">
+			<iframe class="podcast-iframe" src="https://podcasters.spotify.com/pod/show/az-uj-vilag-hangjai/embed/episodes/Nyit-ads-e28r9q3" style="height:100%;width:100%;" frameborder="0" scrolling="no"></iframe>
+		</div>
+	</div>
+
+	<div class="row inner_box narrow-page" style="text-align: left;">
 		<div class="col-sm-12">
-			<div class="inner_box collapse" id="podcast_info" style="font-size: 18px">
-				Ezen az oldalon találhatod "Az új világ hangjai" podcast sorozat beszélgetéseit.<br>
-				Sokféle jövőt elképzelhetünk. Az új világ hangjai hangcsatorna azokat az elképzeléseket gyűjti egybe, amelyekben a világunk egy olyan hely, ahol az emberek biztonságban érzik magukat, felszabadultan önmaguk lehetnek, miközben megvan mindenük ahhoz, hogy életük kiteljesedhessen. Ehhez a kiteljesedéshez arra van szükségük, hogy értéket adjanak a helyi közösségüknek, ami ezáltal megteremti tagjai számára az egészséges, boldog, kiegyensúlyozott élethez szükséges támogatói környezetet. Vagyis társadalmi jóllétet valósítunk meg, együtt. A beszélgetések egy-egy témakört fognak meg ebben a jövőképben.
-			</div>
+				Az új világ hangjai program célja, hogy megmutassuk azoknak az embereknek a jövőképét, akik a társadalmi jóllétért tevékenykednek.<br>
+				A programhoz kapcsolódik egy hangcsatorna, amelynek adásaiban egy-egy ilyen beszélgetőtárs mesél a saját tevékenységéről és jövőképéről. Az adásokhoz nyitott tematikus beszélgetések kapcsolódnak, ahol mindenki, aki érintve érzi magát abban a témában, megmutathatja a saját elképzeléseit, tevékenységét és tapasztalatait, így a beszélgetés résztvevői egymáshoz is kapcsolódni tudnak a téma mentén. A tematikus beszélgetéseket azután újabb találkozó vagy épp konkrét közös munka követheti attól függően, hogy a résztvevőknek mire van igényük.<br>
+				<br>
+				A programhoz többféle módon kapcsolódhatsz:<br>
+				<ul>
+					<li>A hangcsatorna adásában beszélgetőtársként, a saját jövőképedet és tevékenységedet megmutatva</li>
+					<li>A tematikus beszélgetés résztvevőjeként</li>
+					<li>Szervező társként</li>
+					<li>Támogatóként anyagi vagy egyéb felajánlással</li>
+				</ul>
+				Ha szeretnél frissiben értesülni az eseményekről és programokról, a levelezőcsoportunkra itt iratkozhatsz fel: <span style="display: inline-block;">az-uj-vilag-hangjai@googlegroups.com</span><br>
+				<br>
+				A következő elérhetőségeken tudsz kapcsolatba lépni a program szervezőivel: tarsadalmi.jollet@gmail.com | +36302897830 (Pataki Andrea)<br>
+				<br>
+				Itt találod az egyes adásokat, alattuk a kapcsolódó tematikus beszélgetés eseményoldalával, ahol jelentkezni is tudsz a beszélgetésre.<br>
 		</div>
 	</div>
 	<div class="inner_box narrow-page" style="margin-top:6px;">
