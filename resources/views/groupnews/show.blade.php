8f2431976f016283052cf8f70bcbb23d798bcb08
Otto Ludescher <ludescher.otto@gmail.com>
2023-10-28 12:14:01 +0200

    - meta beállítása - módosít gomb a csoport kezelőknek

--- resources/views/groupnews/show.blade.php
+++ resources/views/groupnews/show.blade.php
@@ -2,13 +2,14 @@
 @section('title'){{ $news->title }}@endsection
 @section('description'){{ $news->meta_description }}@endsection
 @section('url'){{url('hir')}}/{{$news->id}}/{{$news->slug}}@endsection
-@section('canonical')<link rel="canonical" href="https://tarsadalmijollet.hu/hirek" />
+@section('canonical')<link rel="canonical" href="{{url('hir')}}/{{$news->id}}/{{$news->slug}}" />@endsection
+@section('image')@if(!empty($news->image)){{url('/')}}/images/posts/{{$news->image}}@else{{url('/images/tarsadalmijollet.png')}}@endif
 @endsection
 
 @section('content')
-	<div class="inner_box" style="font-size: 16px;">
+	<div class="inner_box">
 		<h2>{{ $news->title }}</h2>
-		@if (Auth::check() && Auth::user()->id==$news->user->id)
+		@if ($news->group->isAdmin())
 			<a href="{{url('hir')}}/{{$news->id}}/{{$news->slug}}/modosit" type="submit" class="btn btn-default">Módosít</a>
 		@endif
 		{!! $news->body !!}
