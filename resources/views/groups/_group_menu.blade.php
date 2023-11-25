39aeef8cf38c80f179aa66827d582a817d73c0ca
Otto Ludescher <ludescher.otto@gmail.com>
2023-11-25 22:13:07 +0100

    míg nem oldom meg a bemutatkozás, hírek mindig fog látszani

--- resources/views/groups/_group_menu.blade.php
+++ resources/views/groups/_group_menu.blade.php
@@ -7,9 +7,7 @@
 				@endif
 			</h2>
 			<div class="menu">
-				@if(Auth::guest() && $group->user_visibility=='public' || Auth::check() && ($group->user_visibility=='portal' || $group->isMember()) || $newss->isNotEmpty())
-					<a class="@if ($page=="description") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}">Bemutatkozás</a>
-				@endif
+				<a class="@if ($page=="description") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}">Bemutatkozás</a>
 
 				@if(Auth::guest() && $group->user_visibility=='public' || Auth::check() && ($group->user_visibility=='portal' || $group->isMember()))
 					<a class="@if ($page=="members") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}/tagok">Tagok</a>
@@ -20,9 +18,7 @@
 					<a class="@if ($page=="event") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}/esemenyek">Események</a>
 				@endif
 
-				@if($newss->isNotEmpty())
-					<a class="@if ($page=="news") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}/hirek">Hírek</a>
-				@endif
+				<a class="@if ($page=="news") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}/hirek">Hírek</a>
 
 				@if(Auth::check() && $group->isMember())
 					<a class="@if ($page=="announcement") current @endif" href="{{ url('csoport',$group->id) }}/{{$group->slug}}/kozlemenyek">Közlemények</a>
