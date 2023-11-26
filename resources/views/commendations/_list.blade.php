bf83c702069cca43a5e33952cd1082a1ee6d050e
Otto Ludescher <ludescher.otto@gmail.com>
2023-11-26 23:25:17 +0100

    a kép méret responziv

--- resources/views/commendations/_list.blade.php
+++ resources/views/commendations/_list.blade.php
@@ -39,7 +39,7 @@
         ?>
         <div class="inner_box" style="background-color: #fbfbfb">
             <p><a href="{{ $commendation->url }}" target="_blank">{{ $title }}</a></p>
-            <p><a href="{{ $commendation->url }}" target="_blank"><img src="{{$image}}" height="300px;" style="display: block; margin-left: auto; margin-right: auto;"></a></p>
+            <p><a href="{{ $commendation->url }}" target="_blank"><img src="{{$image}}" style="max-height: 300px; max-width:100%; display: block; margin-left: auto; margin-right: auto;"></a></p>
             <p>@if(strlen($description)>300){{ mb_substr($description,0,300) }} ... @else {{ $description }} @endif</p>
         </div>
 
