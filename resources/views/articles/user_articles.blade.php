9f0442dc92f46946baf1558f195e8f0b97a05541
Otto Ludescher <ludescher.otto@gmail.com>
2023-11-26 10:36:47 +0100

    az inner_box levétele a profil kártyás írásoknál

--- resources/views/articles/user_articles.blade.php
+++ resources/views/articles/user_articles.blade.php
@@ -6,19 +6,17 @@
 
 @section('content')
 	@include('profiles.partials.profile_menu')
-	<div class="inner_box narrow-page" style="margin-top:6px;">
-		@if ($user->isAuthor())
-		<div class="row">
-			<div class="col-sm-9"></div>
-			<div class="col-sm-3 text-right">
-				<a href="#article_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-right: 4px;"></i></a><a href="{{url('iras')}}/uj" type="submit" class="btn btn-default">Új Írás</a>
-			</div>
+	@if ($user->isAuthor())
+	<div class="row">
+		<div class="col-sm-9"></div>
+		<div class="col-sm-3 text-right">
+			<a href="#article_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-right: 4px;"></i></a><a href="{{url('iras')}}/uj" type="submit" class="btn btn-default">Új Írás</a>
 		</div>
-			@include('articles._new_article_info', ['collapse'=>' collapse'])
-		@endif
+	</div>
+		@include('articles._new_article_info', ['collapse'=>' collapse'])
+	@endif
 
-		<div class="row">
-			@include('articles._list')
-		</div>
+	<div class="row">
+		@include('articles._list')
 	</div>
 @endsection
