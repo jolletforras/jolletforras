23727389870ac6d83bd0943903d6dac7e8dc759b
Otto Ludescher <ludescher.otto@gmail.com>
2023-12-17 17:02:20 +0100

    hírlevélnél kiírja a dátumot

--- resources/views/newsletters/_list.blade.php
+++ resources/views/newsletters/_list.blade.php
@@ -3,7 +3,7 @@
         @foreach ($newsletters as $newsletter)
             <tr>
                 <td>
-                    <h3><a href="{{ url('hirlevel',$newsletter->id) }}/{{$newsletter->slug}}">{{ $newsletter->title }}</a></h3>
+                    <h3><a href="{{ url('hirlevel',$newsletter->id) }}/{{$newsletter->slug}}">{{ $newsletter->title }}</a><span style="font-size: 16px; margin-left:10px; margin-right:10px;"> {{$newsletter->created_at}}</span></h3>
                     @if (Auth::check() && Auth::user()->admin)
                         <a href="{{url('hirlevel')}}/{{$newsletter->id}}/{{$newsletter->slug}}/modosit">módosít</a>
                     @endif
