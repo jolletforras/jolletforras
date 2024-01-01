c1d4e816c0ac7189738160b76b09a4f537f7124f
Otto Ludescher <ludescher.otto@gmail.com>
2024-01-01 14:29:00 +0100

    meta az ajánlónál

--- resources/views/commendations/show.blade.php
+++ resources/views/commendations/show.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
+@section('title'){{ $commendation->title }}@endsection
+@section('description'){{$commendation->user->name}} ajánlója - {{ $commendation->meta_title }}@endsection
+@section('url'){{url('alkotas')}}/{{$commendation->id}}/{{$commendation->slug}}@endsection
+@section('canonical')<link rel="canonical" href="{{url('alkotas')}}/{{$commendation->id}}/{{$commendation->slug}}" />@endsection
+@section('image')@if(!empty($commendation->meta_image)){{$commendation->meta_image}}@else{{url('/images/tarsadalmijollet.png')}}@endif
+@endsection
 
 @section('content')
 	<div class="inner_box narrow-page">
