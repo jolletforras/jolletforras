4cc96f78b01ada9223f605e777ad52a147d6cf19
Otto Ludescher <ludescher.otto@gmail.com>
2024-01-08 18:32:28 +0100

    validálás

---
+++ app/Http/Requests/CreationRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules =  [
+				'title' => 'required|max:80',
+				'body' => 'required',
+                'url' => 'required',
+		];
+
+        return $rules;
+    }
+    
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+    	return [
+				'title.required' => 'Kérlek add meg az alkotás címét',
+				'title.max'  => 'A cím legfeljebb :max karakter lehet',
+				'body.required' => 'Kérlek add meg az ajánló szövegét',
+                'url.required' => 'Kérlek add meg a hivatkozást, ahol az alkotásod bemutatod',
+    	];
+    }
+}
