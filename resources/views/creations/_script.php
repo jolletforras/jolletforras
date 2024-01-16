10918990cb4306c13f2ebae913a8de6f0780dd69
Otto Ludescher <ludescher.otto@gmail.com>
2024-01-16 18:29:24 +0100

    alkotásnál csak 5 képet enged  feltölteni

--- resources/views/creations/_script.php
+++ resources/views/creations/_script.php
@@ -1,14 +1,4 @@
 <script type="text/javascript">
-    $(document).ready(function() {
-        if($("#url").is(":checked")) {
-            $("#image_block").hide();
-        }
-        else {
-            $("#url_block").hide();
-        }
-    });
-
-
     function source_change(source){
         if(source=="image") {
             $("#url_block").hide();
