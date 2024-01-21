18a08625384fe69ee4e1e87c66b03c387bb7f143
Otto Ludescher <ludescher.otto@gmail.com>
2024-01-21 18:04:18 +0100

    infóbuborék: Közlemény létrehozáskor minden csoporttag kap emailben értesítést

--- resources/views/groupthemes/create.blade.php
+++ resources/views/groupthemes/create.blade.php
@@ -3,6 +3,16 @@
 @section('content')
 	@include('partials.tinymce_just_link_js')
 	<h2>{{$title}}</h2>
+	@if(isset($announcement))
+		<a href="#create_anoucement_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-right: 4px;"></i></a>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="inner_box collapse" id="create_anoucement_info" style="font-size: 18px">
+					Közlemény létrehozáskor minden csoporttag kap emailben értesítést.<br/>
+				</div>
+			</div>
+		</div>
+	@endif
 	@include('errors.list')
 	<div class="panel panel-default">
 		<div class="panel-body">
