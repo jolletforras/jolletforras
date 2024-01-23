2bec6b2f7a984b8601f0f3b5132d1cea4494b925
Otto Ludescher <ludescher.otto@gmail.com>
2024-01-23 11:12:58 +0100

    felvezető az eseményeknél a vendégek (nem regisztráltak) számára

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 	<div class="row narrow-page">
 		<div class="col-sm-9">
-			<h2>Események</h2><a href="#events_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-left: 4px;"></i></a>
+			<h2>Események</h2>@if (Auth::check())<a href="#events_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-left: 4px;"></i></a>@endif
 		</div>
 		<div class="col-sm-3 text-right">
 			@if (Auth::check())
@@ -15,13 +15,21 @@
 			@endif
 		</div>
 	</div>
-	<div class="row narrow-page">
-		<div class="col-sm-12">
-			<div class="inner_box collapse" id="events_info" style="font-size: 18px">
-				Ezen az oldalon találod a regisztrált tagok és a csoportok nyilvános eseményeit.
-			</div>
-		</div>
+	@if (Auth::check())
+	<div class="inner_box collapse narrow-page" id="events_info">
+			Ezen az oldalon találod a regisztrált tagok és a csoportok nyilvános eseményeit.
 	</div>
+	@endif
+	@guest
+	<div class="inner_box narrow-page">
+		A Társadalmi Jóllét Portálon létrehozhatod és kommunikálhatod az eseményedet, magánszemélyként vagy egy csoport eseményeként.<br>
+		<a href="{{url('tudnivalo')}}/10/esemenyek">ITT</a> találsz erről részletes leírást.<br>
+		<br>
+		Szeretnél értesülni új eseményekről?<br>
+		- Ha van kedved, regisztrálj a Portálon és lépj be ebbe a térbe. Kezdd <a href="{{url('register')}}">ITT</a>.<br>
+		- Ha még csak távolabbról ismerkednél, iratkozz fel a hírlevelünkre <a href="https://forms.gle/S18g4L3TAPC9ZMe99">ITT</a>.<br>
+	</div>
+	@endguest
 	@include('events._create_events_info')
 	<div class="inner_box narrow-page" style="margin-top:6px;">
 		@include('events._list',['events'=>$events])
