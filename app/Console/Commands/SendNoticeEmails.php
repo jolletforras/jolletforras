7cdb591e7ffbdab7d9b67c943fc1495c26c1d47d
Otto Ludescher <ludescher.otto@gmail.com>
2024-01-31 19:39:33 +0100

    24 óráig megnyitható a levélből a bejegyzés, erről tájékoztató szöveg

--- app/Console/Commands/SendNoticeEmails.php
+++ app/Console/Commands/SendNoticeEmails.php
@@ -36,7 +36,7 @@ class SendNoticeEmails extends Command
      */
     public function handle()
     {
-        //addig nem küld ki levelet, amíg a sendemails táblában van kiküldendő levél
+        //!!!!!!addig nem küld ki levelet, amíg a sendemails táblában van kiküldendő levél!!!!!!!!!!
         $sendemails = Sendemail::get();
         if($sendemails->isNotEmpty()) exit;
 
@@ -66,10 +66,10 @@ class SendNoticeEmails extends Command
             $data['group_name'] = $group->name;
             $data['post_title'] = $notifiable->title;
             if($notice->type=="Forum") {
-                $data['post_url'] = 'csoport/' . $group->id . '/' . $group->slug . '/tema/' . $notifiable->id . '/' . $notifiable->slug;
+                $data['post_url'] = 'email/'.$notice->login_code.'/csoport/' . $group->id . '/' . $group->slug . '/tema/' . $notifiable->id . '/' . $notifiable->slug;
             }
             else {
-                $data['post_url'] = 'esemeny/ '. $notifiable->id . '/' . $notifiable->slug;
+                $data['post_url'] = 'email/'.$notice->login_code.'/esemeny/ '. $notifiable->id . '/' . $notifiable->slug;
             }
 
             $data['email'] = $notice->user->email;
--- resources/views/groups/emails/new_comment_email.blade.php
+++ resources/views/groups/emails/new_comment_email.blade.php
@@ -8,4 +8,7 @@ A(z) <b>{{ $group_name }}</b> csoportodban <b><a href="https://tarsadalmijollet.
 <br>
 <a href="https://tarsadalmijollet.hu/{{ $post_url }}" style="margin-top:10px; padding: 8px 12px; background: #f5fcf5; border: 1px solid #464646;border-radius: 5px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #464646;text-decoration: none;font-weight:bold;display: inline-block;">Válaszolok</a>
 <br><br>
-A teljes bejegyzés elolvasásához vagy válaszoláshoz bejelentkezés szükséges. A válaszoláshoz a "Válaszolok" gombra kattints, ha a tarsadalmi.jollet@gmail.com címre válaszolsz, azt a többiek nem fogják látni. Ha nem szeretnél a jövőben értesítéseket kapni, kattints <a href="https://tarsadalmijollet.hu/profilom/beallitasok">IDE</a> és szedd ki az értesítésnél a pipát.
+<b>Egy napig</b> a levélből megnyithatod a bejegyzésed, akkor is, ha előzőleg nem voltál bejelentkezve. Ezért fontos, hogy 1 napig <b>ne továbbítsd</b> a leveledet mások számára, mert akkor ők is ugyanúgy be tudnak lépni a nevedben.
+<b>Egy nap után</b> a teljes bejegyzés elolvasásához vagy válaszoláshoz lehet, hogy be kell jelentkezned.<br>
+A válaszoláshoz a <b>"Válaszolok" gombra</b> kattints! Ha a tarsadalmi.jollet@gmail.com címre válaszolsz, azt a többiek nem fogják látni.
+Ha nem szeretnél a jövőben értesítéseket kapni, kattints <a href="https://tarsadalmijollet.hu/profilom/beallitasok">IDE</a> és vedd ki az értesítésnél a pipát.
