4b85fe843f63875afa94fa2086d420ced8d4e914
Otto Ludescher <ludescher.otto@gmail.com>
2024-02-02 12:43:30 +0100

    ertes=>cimke

--- resources/views/projects/_tags.blade.php
+++ resources/views/projects/_tags.blade.php
@@ -2,8 +2,8 @@
 <p>
 <b>Címkék: </b>
 @for ($i = 0; $i < $last=count($project->tags)-1; $i++)
-<a href="{{ url('kezdemenyezes')}}/ertes/{{$project->tags[$i]->id }}/{{$project->tags[$i]->slug}}">{{ $project->tags[$i]->name }}</a>,
+<a href="{{ url('kezdemenyezes')}}/cimke/{{$project->tags[$i]->id }}/{{$project->tags[$i]->slug}}">{{ $project->tags[$i]->name }}</a>,
 @endfor
-<a href="{{ url('kezdemenyezes')}}/ertes/{{$project->tags[$last]->id }}/{{$project->tags[$last]->slug}}">{{ $project->tags[$last]->name }}</a>
+<a href="{{ url('kezdemenyezes')}}/cimke/{{$project->tags[$last]->id }}/{{$project->tags[$last]->slug}}">{{ $project->tags[$last]->name }}</a>
 </p>
 @endunless
