e078d0d2482219fcd627f16eeb216dccd21ea6c8
Otto Ludescher <ludescher.otto@gmail.com>
2024-02-02 12:15:03 +0100

    a partials.search_tag_script-ből veszi a scriptet

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -61,21 +61,5 @@
 @endsection
 
 @section('footer')
-<script>
-var tags = {
-@foreach ($tags_slug as $id => $slug)
-{{$id}}:"{{$slug}}",
-@endforeach
-};
-
-$('#tag').select2({
-placeholder: 'Keresés címke szerint',
-tags: false
-});
-
-$("#tag").change(function () {
-var id= $("#tag").val();
-location.href="{{ url('kezdemenyezes')}}/cimke/"+id+"/"+tags[id];
-});
-</script>
+	@include('partials.search_tag_script',['url'=>'kezdemenyezes'])
 @endsection
