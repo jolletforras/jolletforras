565124ef0646fc2cc65f6ff7c30e7f93dcb5860e
Otto Ludescher <ludescher.otto@gmail.com>
2024-03-03 15:03:49 +0100

    kezdeményezés felvételnél a felvezető szöveg

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -2,7 +2,33 @@
 
 @section('content')
 	<h2>Kezdeményezés felvétele</h2>
-	
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="inner_box">
+				<b>A kezdeményezés felvételekor a következőkre várjuk a válaszod:</b><br>
+				<ul>
+					<li>Miért indítod ezt a kezdeményezést?</li>
+					<li>Milyen értékeket hordoz vagy támogat a kezdeményezésed?</li>
+					<li>Hogyan szolgálja az új világ építését?</li>
+					<li>Miről szól a kezdeményezés, mi az elképzelésed, megvalósítási terved?</li>
+					<li>Te kezdeményezőként mit tudsz beletenni a megvalósításba?</li>
+				</ul>
+				<b>További tudnivalók</b><br>
+					A kezdeményezés a <a href="{{ url('/') }}/csoport/1/tarsadalmi-jollet-mag" target="_blank">Társadalmi Jóllét Mag</a> jóváhagyása után kerül fel.
+					A jóváhagyás szempontjai: a portál <a href="<?php echo e(url('/')); ?>/kozossegimegallapodas " target="_blank">közösségi megállapodásában</a> megfogalmazott küldetéssel és értékrenddel összhangban van-e,
+					valamint a feltett kérdésekre választ ad-e. Ha az értékrenddel és küldetéssel nincs összhangban, elutasítjuk a megjelenést.
+					Ha a feltett kérdésekre nem ad választ, hiányos, akkor a kiegészítést követően jóváhagyjuk.<br>
+				<br>
+				<b>A kezdeményezés kezelőjének vállalásai:</b><br>
+				<ul>
+					<li>A leírást élőn tartja, frissíti. Ha már nem időszerű, akkor törli a kezdeményezést.</li>
+					<li>Legalább félévente ír egy rövid hírt arról (kezdeményezés/Hírek fül), hol tart a kezdeményezés.</li>
+				</ul>
+				Aki nem teljesíti a vállalását, tudomásul veszi, hogy inaktívba kerül a kezdeményezése addig, míg nem pótolja a beszámolót.<br>
+			</div>
+		</div>
+	</div>
+
 	@include('errors.list')
 	<div class="panel panel-default">
 		<div class="panel-body">
