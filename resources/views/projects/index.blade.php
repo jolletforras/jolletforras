3a80b92fc8da21ace27ca9a1b0ba34d0915e495a
Otto Ludescher <ludescher.otto@gmail.com>
2024-03-03 14:40:49 +0100

    információ buborék a kezdeményezés oldalon belépettek számára

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,13 +4,14 @@
 @section('canonical')<link rel="canonical" href="https://tarsadalmijollet.hu/kezdemenyezesek" /> @endsection
 @section('image'){{ url('/images/kezdemenyezesek.jpg')}}@endsection
 
+<?php $logged_in = Auth::check(); ?>
 @section('content')
 	<div class="row">
 		<div class="col-sm-5">
-			<h2>Kezdeményezések @if(isset($group))- {{$group->name}}@endif</h2>
+			<h2>Kezdeményezések @if(isset($group))- {{$group->name}}@endif</h2>@if($logged_in)<a href="#project_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true" style="margin-left: 4px;"></i></a>@endif
 		</div>
 		<div class="col-sm-3" style="padding-top:4px;">
-			@if(Auth::check())
+			@if($logged_in)
 			<select id="tag" name="tag" class="form-control">
 				@foreach($tags as $key => $val)
 					<option value="{{ $key }}">{{ $val }}</option>
@@ -19,13 +20,13 @@
 			@endif
 		</div>
 		<div class="col-sm-4 text-right">
-			@if(Auth::check())
+			@if($logged_in)
 			<a href="{{url('kezdemenyezes')}}/uj" type="submit" class="btn btn-default">Új kezdeményezés</a>
 			@endif
 		</div>
 	</div>
-	@guest
-	<div class="inner_box">
+
+	<div class="inner_box @if($logged_in)collapse @endif" id="project_info">
 		Itt a Portál tagjainak saját kezdeményezéseit találod. A kezdeményezés lehet:<br>
 		<ul>
 			<li> egy ötletet valaki vagy valakik indulásra késznek gondolnak, akár már el is kezdték a megvalósítását vagy előkészítését, ám még társakat várnak ahhoz, hogy a kezdeményezés meg is valósuljon, működjön vagy fenntarthatóan fejlődhessen</li>
@@ -33,18 +34,18 @@
 		</ul>
 
 		Arról, hogyan hozhatsz létre új kezdeményezést és hogyan tudsz kezdeményezésekhez kapcsolódni, <a href="{{url('tudnivalo')}}/12/kezdemenyezesek">ITT</a> találsz részletes tudnivalókat.<br>
+
+		@guest
 		<br>
 		Szeretnél értesülni új kezdeményezésekről?<br>
 		- Ha van kedved, regisztrálj a Portálon és lépj be ebbe a térbe. Kezdd <a href="{{url('register')}}">ITT</a>.<br>
 		- Ha még csak távolabbról ismerkednél, iratkozz fel a hírlevelünkre <a href="https://forms.gle/S18g4L3TAPC9ZMe99">ITT</a>.<br>
+		@endguest
 	</div>
-	@endguest
+
 	<hr style="margin-top:6px;">
 	<div class="row">
-	<?php
-		$i=1;
-		$logged_in = Auth::check();
-	?>
+	<?php $i=1;	?>
 	@foreach ($projects as $project)
 		<?php
 			$project_admin = $project->isAdmin();
