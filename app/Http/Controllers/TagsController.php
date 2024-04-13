35ff2173b1b03e4b3f4e14146d767c841e6b7ec1
Otto Ludescher <ludescher.otto@gmail.com>
2024-04-13 14:10:41 +0200

    a Group modell hozzáadása

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Group;
 use App\Models\ForumTag;
 use App\Models\GroupTag;
 use App\Models\Groupnews;
