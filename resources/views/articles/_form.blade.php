ebf7248c5bae79d40b4c86a97208d3a7b030d830
Otto Ludescher <ludescher.otto@gmail.com>
2024-08-25 10:22:10 +0200

    Meta leírás infó buborék kiegészítése

--- resources/views/articles/_form.blade.php
+++ resources/views/articles/_form.blade.php
@@ -8,7 +8,7 @@
 				<div class="form-group">
 					<label for="meta_description">Meta leírás:</label>
 					<a href="#meta_description_info" data-toggle="collapse"><i class="fa fa-info-circle" aria-hidden="true"></i></a></label>
-					<div id="meta_description_info" class="collapse info">Az itt megadott szöveg íródik ki ha egy másik oldalon (pl. Facebook) megosztásra kerül az írás.</div>
+					<div id="meta_description_info" class="collapse info">Az itt megadott szöveg íródik ki ha egy másik oldalon (pl. Facebook) megosztásra kerül az írás. Ennek akkor van jelentősége, ha nyilvános az írás, vagyis nem regisztráltak számára is elérhető.</div>
 					<input class="form-control" required="required" name="meta_description" type="text" maxlength="160" value="@if(isset($article)){{$article->meta_description}} @endif" id="meta_description">
 				</div>
 
