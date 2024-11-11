1f013534d1b59ed0c1e162255cb8bcfa6306d855
ludeschero <ludescher.otto@gmail.com>
2024-11-11 15:25:48 +0100

    meta adatok

--- resources/views/articlecategories/show.blade.php
+++ resources/views/articlecategories/show.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
+@section('title'){{ $category->title }}@endsection
+@section('description'){{ $category->meta_description }}@endsection
+@section('url'){{url('irasok')}}/{{$category->id}}/{{$category->slug}}@endsection
+@section('canonical')<link rel="canonical" href="{{url('irasok')}}/{{$category->id}}/{{$category->slug}}"  />@endsection
+@section('image'){{ url('/images/categories') }}/{{ $category->id.'.jpg?'.$category->photo_counter}}@endsection
 
 @section('content')
 	<div class="inner_box narrow-page" style="margin-top:6px;">
 		<h2>{{ $category->title }}</h2>
 		@if (Auth::check() && Auth::user()->id==$category->user->id)
-			<a href="{{url('temakor')}}/{{$category->id}}/{{$category->slug}}/modosit" type="submit" class="btn btn-default"><i class="fa fa-edit" aria-hidden="true"> </i>Módosít</a>
+			<a href="{{url('iras-temakor')}}/{{$category->id}}/{{$category->slug}}/modosit" type="submit" class="btn btn-default"><i class="fa fa-edit" aria-hidden="true"> </i>Módosít</a>
 			<a href="{{url('temakor')}}/{{$category->id}}/{{$category->slug}}/kepfeltoltes" type="submit" class="btn btn-default">Képfeltöltés</a>
 		@endif
 		@if(file_exists(public_path('images/categories/'.$category->id.'.jpg')))
